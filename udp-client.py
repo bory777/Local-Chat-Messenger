94a8d5d9749f9ef052b6e5f6c8938c9a99147e34
bori <au08050540977@gmail.com>
2024-06-10 09:41:09 +0000

    Update

--- udp-client.py
+++ udp-client.py
@@ -1,39 +1,39 @@
 import socket
 import os
-from faker import Faker
 
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
 server_address = '/tmp/udp_socket_file'
 
-address = '/tmp/udp_client_socket_file'
+client_address = '/tmp/udp_client_socket_file'
 
-fake = Faker()
-fake_name = fake.name()
-fake_address = fake.address()
 
-print('ターミナルからの入力をfakerにやってもらいます。')
-print(f"生成された名前：{fake_name}")
-print(f"生成されたアドレス：{fake_address}")
-
-message = f"名前：{fake_name}, アドレス：{fake_address}"
 
 try:
-    os.unlink(address)
+    os.unlink(client_address)
 except FileNotFoundError:
     pass
-sock.bind(address)
+sock.bind(client_address)
 
 try:
-    print('{!r}を送ります。'.format(message))
-    sent = sock.sendto(message.encode(), server_address)
+    while True:
+        print('メッセージを入力してください。終了するには"exit"と入力してください。')
+        message = input()
 
-    print('応答を待っています。')
 
-    data, server = sock.recvfrom(4096)
+        print(f"送信メッセージ：{message}")
+        sent = sock.sendto(message.encode(), server_address)
+        if message.strip().lower() == 'exit':
+            break
 
-    print('{!r}を受け取りました。'.format(message))
+        print('応答を待っています。')
+
+        data, server = sock.recvfrom(4096)
+
+        print('{!r}を受け取りました。'.format(data.decode()))
 
 finally:
     print('ソケットを閉じます。')
     sock.close()
+    if os.path.exists(client_address):
+        os.unlink(client_address)
--- udp-server.py
+++ udp-server.py
@@ -1,8 +1,14 @@
 import socket
 import os
+from faker import Faker
 
+# ソケットの作成
+# UNIXドメインソケットを使用
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 server_address = '/tmp/udp_socket_file'
+
+# Fakerオブジェクトの作成
+fake = Faker()
 
 try:
     os.unlink(server_address)
@@ -13,14 +19,30 @@ except FileNotFoundError:
 print('{}を起動します。'.format(server_address))
 sock.bind(server_address)
 
-while True:
-    print('\nメッセージを待っています。')
+try:
+    while True:
+        #クライアントからのメッセージを受信し出力
+        print('\nメッセージを待っています。')
+        data, address = sock.recvfrom(4096)
 
-    data, address = sock.recvfrom(4096)
+        print('{}から{}バイト受け取りました。'.format(address, len(data)))
+        print('受信データ：{}'.format(data.decode()))
+        
+        # ランダムな文字列を生成しクライアントに送信
+        if data:
+            if data.decode().strip().lower() == 'exit':
+                print('サーバーを終了します。')
+                break
 
-    print('{}から{}バイト受け取りました。'.format(address, len(data)))
-    print(data)
+            else:
+                random_text = fake.lexify(text = "????????")
+                message = f"ランダムな文字列：{random_text}"
+                print('応答メッセージ：{}'.format(message))
+                sock.sendto(message.encode(), address)
 
-    if data:
-        sent = sock.sendto(data, address)
-        print('{}から{}バイト送り返しました。'.format(address, sent))
+finally:
+    print('ソケットを閉じます。')
+    sock.close()
+    if os.path.exists(server_address):
+        os.unlink(server_address)
+
